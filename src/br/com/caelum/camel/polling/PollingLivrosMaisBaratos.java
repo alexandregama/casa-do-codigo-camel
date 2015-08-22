3a3fa7058f54382355fcf725ede041d6a4b83379
alexandregama <alexandre.gama.lima@gmail.com>
2015-08-22 07:24:03 -0300

    Adiciona polling dos livros mais baratos, enviando para o banco de dados o Autor

--- src/br/com/caelum/camel/polling/PollingLivrosMaisBaratos.java
+++ src/br/com/caelum/camel/polling/PollingLivrosMaisBaratos.java
@@ -11,6 +11,9 @@ import org.apache.camel.Message;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.util.jndi.JndiContext;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlConnectionPoolDataSource;
 
 import br.com.caelum.livraria.modelo.Livro;
 
@@ -20,7 +23,17 @@ public class PollingLivrosMaisBaratos {
 	public static void main(String[] args) throws Exception {
 		System.out.println("Iniciando o polling dos livros mais baratos");
 		
-		CamelContext context = new DefaultCamelContext();
+		MysqlConnectionPoolDataSource mysqlDs = new MysqlConnectionPoolDataSource();
+		mysqlDs.setServerName("localhost");
+		mysqlDs.setPort(3306);
+		mysqlDs.setDatabaseName("camel");
+		mysqlDs.setUser("root");
+		mysqlDs.setPassword("");
+		
+		JndiContext jndi = new JndiContext();
+		jndi.rebind("mysqlDataSource", mysqlDs);
+		
+		CamelContext context = new DefaultCamelContext(jndi);
 		
 		context.addRoutes(new RouteBuilder() {
 			
@@ -45,7 +58,24 @@ public class PollingLivrosMaisBaratos {
 						System.out.println("Livros mais baratos: " + livros);
 					}
 				}).
-				to("mock:livros");
+				to("direct:livros");
+				
+				from("direct:livros").
+					log(LoggingLevel.INFO, "Enviando para o banco de dados").
+						split(body()).
+				process(new Processor() {
+					
+					@Override
+					public void process(Exchange exchange) throws Exception {
+						Message message = exchange.getIn();
+						
+						Livro livro = (Livro) message.getBody();
+						
+						message.setHeader("autor", livro.getNomeAutor());
+					}
+				}).
+				setBody(simple("insert into Livros (nomeAutor) values ('${header[autor]}')")).
+				to("jdbc:mysqlDataSource");
 			}
 		});
 		
